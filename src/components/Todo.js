e6baadeba36b97608ffb303b2729375908cd77a0
Rayyan Rahman <129946782+RayyanRahman@users.noreply.github.com>
2024-01-18 21:41:21 +0530

    Update Todo.js
    Sort functionality added and fetching of todos from firebase moved to todo component from app component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { updateDoc, deleteDoc, doc } from "firebase/firestore";
+import {
+  updateDoc,
+  deleteDoc,
+  doc,
+  collection,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
-function Todo({ todo }) {
+function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+
+      // Sort todosArray based on lastUpdatedTime in descending order
+      todosArray.sort((a, b) => b.lastUpdatedTime - a.lastUpdatedTime);
+      setTodos(todosArray);
+    });
+
+    // Cleanup function to unsubscribe from snapshot listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
   // Function for deleting the todo task
   const handleDelete = async (id) => {
     await deleteDoc(doc(db, "todos", id));
   };
+
   // Function for handling todo task completion
   const handleComplete = async (todo) => {
     let currentTime = Date.now();
@@ -17,27 +45,36 @@ function Todo({ todo }) {
       lastUpdatedTime: currentTime,
     });
   };
-  return (
-    <div className="todo">
-      <p
-        style={{ textDecoration: todo.completed && "line-through" }}
-        className="list"
-      >
-        {todo.task}
-      </p>
-      <div>
-        <button
-          className="button-complete"
-          onClick={() => handleComplete(todo)}
-        >
-          <CheckCircleIcon className="icons" />
-        </button>
 
-        <button className="button-delete" onClick={() => handleDelete(todo.id)}>
-          <DeleteIcon className="icons" />
-        </button>
-      </div>
+  return (
+    <div className="todo-container">
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <p
+            style={{ textDecoration: todo.completed && "line-through" }}
+            className="list"
+          >
+            {todo.task}
+          </p>
+          <div>
+            <button
+              className="button-complete"
+              onClick={() => handleComplete(todo)}
+            >
+              <CheckCircleIcon className="icons" />
+            </button>
+
+            <button
+              className="button-delete"
+              onClick={() => handleDelete(todo.id)}
+            >
+              <DeleteIcon className="icons" />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
+
 export default Todo;
