b60f05b854e4b095e4eb55e40e9df85b78161f31
Rayyan Rahman <129946782+RayyanRahman@users.noreply.github.com>
2024-01-18 21:26:21 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,22 @@
 import React from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { updateDoc, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
 
-function Todo({ todo, handleComplete, handleDelete }) {
+function Todo({ todo }) {
+  // Function for deleting the todo task
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+  // Function for handling todo task completion
+  const handleComplete = async (todo) => {
+    let currentTime = Date.now();
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+      lastUpdatedTime: currentTime,
+    });
+  };
   return (
     <div className="todo">
       <p
@@ -16,11 +30,11 @@ function Todo({ todo, handleComplete, handleDelete }) {
           className="button-complete"
           onClick={() => handleComplete(todo)}
         >
-          <CheckCircleIcon id="i" />
+          <CheckCircleIcon className="icons" />
         </button>
 
         <button className="button-delete" onClick={() => handleDelete(todo.id)}>
-          <DeleteIcon id="i" />
+          <DeleteIcon className="icons" />
         </button>
       </div>
     </div>
