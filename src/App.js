567943e8a8ab2f68b1459ef44a3fa73da6fefe10
Rayyan Rahman <129946782+RayyanRahman@users.noreply.github.com>
2024-01-18 21:25:38 +0530

    Sort Functionality Added

--- src/App.js
+++ src/App.js
@@ -2,57 +2,45 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
-import {
-  collection,
-  query,
-  onSnapshot,
-  doc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
+import { collection, query, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 
 function App() {
   // State for managing the list of todos
   const [todos, setTodos] = useState([]);
 
-   // useEffect to fetch todos from Firebase on component mount
+  // useEffect to fetch todos from Firebase on component mount
   useEffect(() => {
     const q = query(collection(db, "todos"));
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let todosArray = [];
       querySnapshot.forEach((doc) => {
         todosArray.push({ ...doc.data(), id: doc.id });
       });
+
+      // Sort todosArray based on lastUpdatedTime in descending order
+      todosArray.sort((a, b) => b.lastUpdatedTime - a.lastUpdatedTime);
       setTodos(todosArray);
     });
-    // Cleanup function to unsubscribe from snapshot listener
+
+    // Cleanup function to unsubscribe from snapshot listener when the component unmounts
     return () => unsubscribe();
   }, []);
-
-  // Function for handling todo task completion
-  const handleComplete = async (todo) => {
-    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
-  };
-
-  // Function for deleting the todo task
-  const handleDelete = async (id) => {
-    await deleteDoc(doc(db, "todos", id));
-  };
 
   // Render the main structure of the App
   return (
     <div className="App">
-      <div className="addTodoCont">
+      <div className="add-todo-cont">
         <AddTodo />
       </div>
-      <div className="todo_container">
+      <div className="todo-container">
         {todos.map((todo) => (
           <Todo
             key={todo.id}
             todo={todo}
-            handleComplete={handleComplete}
-            handleDelete={handleDelete}
+            // handleComplete={handleComplete}
+            // deleteDoc={deleteDoc}
           />
         ))}
       </div>
